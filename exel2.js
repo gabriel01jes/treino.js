d0932e338773208e164363fbaf549b1a28c63f34
gabriel01jes <gabriel.jesuz.santos@escola.pr.gov.br>
2025-10-20 09:35:45 -0300

    update

---
+++ exel2.js
@@ -0,0 +1,8 @@
+console.log("Operações aritmetricas");
+
+console.log(' soma:, 3+2');
+console.log(' subtrção:', 3-2);
+console.log("divisão:", 3/2);
+console.log('multiplicação:', 3*2);
+console.log('resto da divisão', 3%2);
+
