0cbc0f6b50029757d0e719d5b8d0111458cb8224
gabriel01jes <gabriel.jesuz.santos@escola.pr.gov.br>
2025-10-20 09:27:16 -0300

    update

---
+++ exe1.js
@@ -0,0 +1,8 @@
+console.log("hello, word");
+
+
+// como declarar valores na javascript
+
+let valor; // Declaram variaveis 
+const valor2= "Nunca vai ser alterada"; // Declaramos uma variavel constante
+valor
